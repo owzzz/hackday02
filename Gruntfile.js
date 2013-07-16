1556dcbc2d858a809a9959814ebc8094a826e0ed
Owain Llewellyn <owain.llew@gmail.com>
2013-07-16 10:46:06 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+'use strict';
+ 
+module.exports = function (grunt) {
+ 
+ 
+  // set up config
+  grunt.initConfig({
+ 
+  });
+ 
+  // register a few tasks
+  grunt.registerTask('default', []);
+};
